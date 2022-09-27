b0378b261eb27159ab577ccb08ef6446bc894a12
Diogo R Roessler <drroessler@hotmail.com.br>
2022-09-27 16:38:19 -0300

    Fix something on comments and, addon.

--- startapp/databases/postgresql/pg_user_db.py
+++ startapp/databases/postgresql/pg_user_db.py
@@ -3,6 +3,19 @@ from psycopg2 import errors
 
 
 def create_new_cmd_pg(query):
+    """
+    Create Command-Line for query and commit sequence to Postgres.
+
+    Args:
+        query: Is a Command-Line.
+
+    See:
+        DuplicateTable removed that sequence.
+
+    Returns:
+        It's returns an connection from psycopg2.connection
+    """
+
     new_instance = psycopg2.connect(dbname='myuserdb', host='localhost', user='root', password='root', port='5432')
     if new_instance:
         print('Connection ready with Postgresql.')
